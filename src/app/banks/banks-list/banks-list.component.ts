c515a270cac281086a9f1ccd3c4e2382c889365d
Sameer Al Harbi <smrhrbi@gmail.com>
2019-09-25 02:47:27 +0300

    services

--- src/app/banks/banks-list/banks-list.component.ts
+++ src/app/banks/banks-list/banks-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Bank } from '../bank.model';
+import { BanksService } from '../banks.service';
 
 @Component({
   selector: 'app-banks-list',
@@ -11,19 +12,17 @@ export class BanksListComponent implements OnInit {
   banks: Bank[] = [];
   creating = false;
 
-  constructor() { }
+  constructor(private banksService: BanksService) {
+
+   }
 
   ngOnInit() {
-    this.banks = [
-      new Bank('0001', 'AlAhli', '1234', undefined,
-       'https://lh3.googleusercontent.com/f5-j7DPz58-qL8tUqrTj50wjaxSImvLalTDjIpOdfQkM9XMh9r_D9IjnvrNyr8QPy9bU'),
-      new Bank('0002', 'BAJ', '1235', undefined,
-        'http://www.aleqt.com/sites/default/files/rbitem/2018/04/8/810791-1878954177.png'),
-      new Bank('0001', 'SAMBA', '1236', undefined,
-        'https://pbs.twimg.com/amplify_video_thumb/1121016061149814785/img/btEi3O52INNh96DH.jpg'),
-      new Bank('0001', 'AlFransi'
-      , '1237', undefined, ''),
-    ];
+    this.banks = this.banksService.getBanks();
+    this.banksService.banksChanged.subscribe(
+      (banksList: Bank[]) => {
+        this.banks = banksList;
+      }
+    );
   }
 
   onNewBanks() {
--- src/app/banks/banks.component.ts
+++ src/app/banks/banks.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Bank } from './bank.model';
+import { BanksService } from './banks.service';
 
 @Component({
   selector: 'app-banks',
   templateUrl: './banks.component.html',
-  styleUrls: ['./banks.component.css']
+  styleUrls: ['./banks.component.css'],
+  providers: [BanksService]
 })
 export class BanksComponent implements OnInit {
 
